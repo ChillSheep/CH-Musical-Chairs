652dc11022a33587a9983a52f5d4f186e75d0918
ChillSheep <52506697+ChillSheep@users.noreply.github.com>
2019-12-16 08:46:21 -0800

    Add files via upload

---
+++ Musical Chairs/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Musical_Chairs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(0, -1);
+            this.label2.MaximumSize = new System.Drawing.Size(2920, 2080);
+            this.label2.MinimumSize = new System.Drawing.Size(2920, 2080);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(2920, 2080);
+            this.label2.TabIndex = 1;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Restart Song";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Scaunele Muzicale";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Musical Chairs/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace Musical_Chairs
+{
+    public partial class Form1 : Form
+    {
+        public WaveOutEvent outputDevice;
+        public AudioFileReader audioFile;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+        {
+            outputDevice.Dispose();
+            outputDevice = null;
+            audioFile.Dispose();
+            audioFile = null;
+        }
+        bool change = true;
+        private void label2_Click(object sender, EventArgs e)
+        {
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += OnPlaybackStopped;
+            }
+            if (audioFile == null)
+            {
+                try {
+                    audioFile = new AudioFileReader(@"song.mp3");
+                    outputDevice.Init(audioFile);
+                }
+                catch { MessageBox.Show("You need to put the file song.mp3 in the software folder"); Application.Exit(); }
+            }
+            if (change==true)
+            {
+                label2.BackColor = Color.FromArgb(20, 50, 200);
+                change = false;
+                button1.Visible = false;
+                outputDevice.Play();
+            }
+            else
+            {
+                label2.BackColor = Color.FromArgb(0, 0, 0);;
+                change = true;
+                outputDevice.Pause();
+                button1.Visible = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            audioFile = null;
+            outputDevice = null;
+        }
+    }
+}
